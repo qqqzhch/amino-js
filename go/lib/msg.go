792d41f0ccd8e8d9442d904ad1ec8bb22ff9c4c4
zhangchao <544262408@qq.com>
2019-09-19 08:40:54 +0800

    test

--- go/lib/msg.go
+++ go/lib/msg.go
@@ -2,39 +2,25 @@ package lib
 
 import (
 	sdk "github.com/cosmos/amino-js/go/lib/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/amino-js/go/lib/tendermint/tendermint/crypto"
+	
 )
 
 var (
-	_ sdk.Msg = &MsgCreateValidator{}
-	_ sdk.Msg = &MsgEditValidator{}
+	
 	_ sdk.Msg = &MsgDelegate{}
 	_ sdk.Msg = &MsgUndelegate{}
 	_ sdk.Msg = &MsgBeginRedelegate{}
 )
 
-type MsgCreateValidator struct {
-	Description       Description    `json:"description"`
-	Commission        CommissionMsg  `json:"commission"`
-	MinSelfDelegation sdk.Int        `json:"min_self_delegation"`
-	DelegatorAddress  sdk.AccAddress `json:"delegator_address"`
-	ValidatorAddress  sdk.ValAddress `json:"validator_address"`
-	PubKey            crypto.PubKey  `json:"pubkey"`
-	Value             sdk.Coin       `json:"value"`
-}
 
-type MsgEditValidator struct {
-	Description
-	ValidatorAddress sdk.ValAddress `json:"address"`
 
-	CommissionRate    *sdk.Dec `json:"commission_rate"`
-	MinSelfDelegation *sdk.Int `json:"min_self_delegation"`
-}
+
 
 type MsgDelegate struct {
 	DelegatorAddress sdk.AccAddress `json:"delegator_address"`
 	ValidatorAddress sdk.ValAddress `json:"validator_address"`
 	Amount           sdk.Coin       `json:"amount"`
+	ValidatorType    *sdk.Int       `json:"validator_type"`
 }
 
 type MsgBeginRedelegate struct {
